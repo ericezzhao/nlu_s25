2431327d64138f6df4834076fb53f9437da00a80
Eric Zhao <90228788+ericezzhao@users.noreply.github.com>
2025-03-24 17:10:35 -0700

    yipee

--- hw3/hpc/run_truthfulqa_evaluation.sh
+++ hw3/hpc/run_truthfulqa_evaluation.sh
@@ -1,7 +1,7 @@
 # Add all your Python calls here:
 # python /scratch/ez2545/nlu_s25/hw3/truthfulqa.py facebook/opt-125m
 # python /scratch/ez2545/nlu_s25/hw3/truthfulqa.py facebook/opt-350m
-python /scratch/ez2545/nlu_s25/hw3/truthfulqa.py facebook/opt-1.3b
-#python /scratch/ez2545/nlu_s25/hw3/truthfulqa.py facebook/opt-2.7b
+# python /scratch/ez2545/nlu_s25/hw3/truthfulqa.py facebook/opt-1.3b
+python /scratch/ez2545/nlu_s25/hw3/truthfulqa.py facebook/opt-2.7b
 #python /scratch/ez2545/nlu_s25/hw3/truthfulqa.py facebook/opt-6.7b
 # ... etc.
